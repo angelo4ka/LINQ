08eb983aecea678d7ea9e20863cba058cbf91830
angelo4ka <karina3125@yandex.ru>
2022-02-10 15:13:38 +0300

    Добавлен интерфейс (общение с пользователем), а также реализован ввод буквы, по которой происходит выборка.

--- BasicsOfLINQ/Program.cs
+++ BasicsOfLINQ/Program.cs
@@ -11,11 +11,16 @@ namespace BasicsOfLINQ
         static void Main(string[] args)
         {
             string[] teams = { "Бавария", "Боруссия", "Реал Мадрид", "Манчестер Сити", "ПСЖ", "Барселона" };
-            
-            Scoring(teams);
-            SelectionWithLINQ(teams);
+            string character;
 
-            Console.Write("Конец программы. Для выхода из неё нажмите любую клавишу на клавиатуре.");
+            Console.Write("Введите букву команды, по которой будет происходить выборка (регистр не имеет значения): ");
+            character = Console.ReadLine();
+            character = character.ToUpper();
+
+            Scoring(teams, character);
+            SelectionWithLINQ(teams, character);
+
+            Console.WriteLine("Конец программы. Для выхода из неё нажмите любую клавишу на клавиатуре.");
             Console.ReadKey();
         }
 
@@ -23,12 +28,12 @@ namespace BasicsOfLINQ
         /// Обычная (стандартная) выборка из массива строки, начинающиеся на определенную букву и сортировка полученного списка
         /// </summary>
         /// <param name="teams">Массив строк для выборки</param>
-        public static void StandardSelection(string[] teams)
+        public static void StandardSelection(string[] teams, string character)
         {
             var selectedTeams = new List<string>();
             foreach (string s in teams)
             {
-                if (s.ToUpper().StartsWith("Б"))
+                if (s.ToUpper().StartsWith(character))
                     selectedTeams.Add(s);
             }
             selectedTeams.Sort();
@@ -42,10 +47,10 @@ namespace BasicsOfLINQ
         /// Выборка с помощью LINQ из массива строки, начинающиеся на определенную букву и сортировка полученного списка
         /// </summary>
         /// <param name="teams">Массив строк для выборки</param>
-        public static void SelectionWithLINQ(string[] teams)
+        public static void SelectionWithLINQ(string[] teams, string character)
         {
             var selectedTeams = from t in teams // Определяем каждый объект из teams как t
-                                where t.ToUpper().StartsWith("Б") // Фильтрация по критерию
+                                where t.ToUpper().StartsWith(character) // Фильтрация по критерию
                                 orderby t  // Упорядочиваем по возрастанию
                                 select t; // Выбираем объект
 
@@ -58,7 +63,7 @@ namespace BasicsOfLINQ
         /// Выборка с помощью методов расширения LINQ из массива строки, начинающиеся на определенную букву и сортировка полученного списка
         /// </summary>
         /// <param name="teams">Массив строк для выборки</param>
-        public static void SelectionWithLINQExtensionMethods(string[] teams)
+        public static void SelectionWithLINQExtensionMethods(string[] teams, string character)
         {
             var selectedTeams = teams.Where(t => t.ToUpper().StartsWith("Б")).OrderBy(t => t);
 
@@ -71,12 +76,15 @@ namespace BasicsOfLINQ
         /// Подсчёт количества результатов выборки
         /// </summary>
         /// <param name="teams">Массив строк для выборки</param>
-        public static void Scoring(string[] teams)
+        public static void Scoring(string[] teams, string character)
         {
             int number = (from t in teams
-                          where t.ToUpper().StartsWith("Б")
+                          where t.ToUpper().StartsWith(character)
                           select t).Count();
-            Console.WriteLine($"Найдено: {number}\n=====================");
+            
+            if (number != 0)
+                Console.WriteLine($"Найдено: {number}\n=====================");
+            else Console.WriteLine("Результат: по Вашему запросу ничего не найдено.");
         }
     }
 }
