073a37eea4c8f711287247d4739a4ac7f9a51dc2
angelo4ka <karina3125@yandex.ru>
2022-02-11 16:05:30 +0300

    Добавлен класс User (Пользователь)

---
+++ BasicsOfLINQ/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsOfLINQ
+{
+    /// <summary>
+    /// Класс "Пользователь"
+    /// </summary>
+    class User
+    {
+        // Свойства класса
+        public string Name { get; set; } // Имя
+        public int Age { get; set; } // Возраст
+        public List<string> Languages { get; set; } // Языки
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public User()
+        {
+            // Для объекта "Языки" создаётся новый список
+            Languages = new List<string>();
+        }
+    }
+}
