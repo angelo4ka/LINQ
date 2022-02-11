71511a629fdbe853e3b5e6f6e4e3001959b75073
angelo4ka <karina3125@yandex.ru>
2022-02-11 19:19:28 +0300

    Добавлен класс Phone (телефон)

---
+++ BasicsOfLINQ/Phone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsOfLINQ
+{
+    /// <summary>
+    /// Класс "Телефон"
+    /// </summary>
+    class Phone
+    {
+        // Свойства класса
+        public string Name { get; set; } // Название
+        public string Company { get; set; } // Компания
+    }
+}
